c40f9210b9e99f379606b55d178825922c0cd5f5
RISE-Fork-Updater <rise-fork-updater@asml.com>
2025-08-01 06:09:17 +0000

    Rewrite the docker registry to artifactory

--- docker/Dockerfile.swift
+++ docker/Dockerfile.swift
@@ -1,1 +1,1 @@
-FROM ghcr.io/dependabot/dependabot-updater-swift:v2.0.20250708095246@sha256:23b0435636a0b87ed10661444827d5d384973eae0946160e546019fc776521ae
+FROM reg-ghcrio.artifactory-de.asml.com/dependabot/dependabot-updater-swift:v2.0.20250708095246@sha256:23b0435636a0b87ed10661444827d5d384973eae0946160e546019fc776521ae
